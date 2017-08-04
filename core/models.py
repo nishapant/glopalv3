c3a74c1fe49beef94b0d26f12970afb59b695660
Nadean <nadeanghouleh@yahoo.com>
2017-08-04 15:49:31 -0500

    MAJOR BREAKTHROUGH

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+    taskName = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.taskName + ' ' + self.category
+#    points = models.CharField(max_length=10)
--- glopalv3/urls.py
+++ glopalv3/urls.py
@@ -5,5 +5,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^core/', include('core.urls')),
+    url(r'^core/', include('core.urls'))
 ]
