b0e710fa6071971cf96a4cf547bd4e35ae8b42eb
Nadean <nadeanghouleh@yahoo.com>
2017-08-07 10:02:11 -0500

    simplify code

--- core/views.py
+++ core/views.py
@@ -1,18 +1,16 @@
 
 #from django.shortcuts import render
 
+
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from .models import Activity
+
 
 def index(request):
     all_activites = Activity.objects.all()
-    template = loader.get_template('core/index.html')
-
-    context = {
-        'all_activites': all_activites,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'core/index.html', {'all_activites': all_activites})
 
 def detail(request, activity_id):
-    return HttpResponse('<h2>Details for activity id: ' + str(activity_id) + '</h2>')
+    activity = get_object_or_404(Activity, pk=activity_id)
+    return render(request, 'core/detail.html', {'activity': activity})
