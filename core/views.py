3ce803155ddf12aa4e80dce967674e1d445c617f
Pawandeep Kaur <pkaur1223@gmail.com>
2017-08-07 11:00:24 -0500

    started user login

--- core/views.py
+++ core/views.py
@@ -5,6 +5,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Activity
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.views.generic import View
+from .forms import UserForm
 
 
 def index(request):
@@ -14,3 +19,7 @@ def index(request):
 def detail(request, activity_id):
     activity = get_object_or_404(Activity, pk=activity_id)
     return render(request, 'core/detail.html', {'activity': activity})
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'core/registration_form.html'
