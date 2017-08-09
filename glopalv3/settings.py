420aefc3ce7ea87ef8c5b19cde12469fd05a7eb0
Nisha Pant <nisha007pant@gmail.com>
2017-08-09 15:00:12 -0500

    update

--- glopalv3/settings.py
+++ glopalv3/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mathfilters',
 ]
 
 # AUTH_PROFILE_MODULE = 'accounts.UserProfile'
